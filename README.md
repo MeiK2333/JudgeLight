42b89601cece01ac59b3c3afbc04566b4dfa7c32
MeiK <meik2333@gmail.com>
2018-11-06 16:15:21 +0800

    Update

--- README.md
+++
@@ -1,8 +0,0 @@
-# JudgeLight
-
-
-## rely
-
-- [python3](https://www.python.org/)
-- [redis](https://redis.io/)
-- [plunge](https://github.com/MeiK-h/plunge)
--- web.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-from flask import Flask
-
-from config import SYSTEM_CONFIG
-
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run(debug=True, host=SYSTEM_CONFIG['flask_host'], port=SYSTEM_CONFIG['flask_port'])

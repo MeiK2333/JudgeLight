bf4557d27783c0a331a5f5b2fe223c77149fa1cf
MeiK <meik2333@gmail.com>
2019-02-10 18:34:28 +0800

    不解析不需要的数据，从而加快评测速度

--- JudgeLight/JudgeLightRunner/jl_memory.c
+++ JudgeLight/JudgeLightRunner/jl_memory.c
@@ -27,19 +27,22 @@ int GetMemoryUsage(pid_t pid, struct MemoryStatus *ms) {
         if (buf[0] != 'V') {
             continue;
         }
-        if (strncmp(buf, "VmSize:", 7) == 0) {
-            sscanf(buf + 7, "%d", &ms->vm_size);
-        } else if (strncmp(buf, "VmRSS:", 6) == 0) {
+        // if (strncmp(buf, "VmSize:", 7) == 0) {
+        //     sscanf(buf + 7, "%d", &ms->vm_size);
+        // } else
+        if (strncmp(buf, "VmRSS:", 6) == 0) {
             sscanf(buf + 6, "%d", &ms->vm_rss);
-        } else if (strncmp(buf, "VmData:", 7) == 0) {
-            sscanf(buf + 7, "%d", &ms->vm_data);
-        } else if (strncmp(buf, "VmStk:", 6) == 0) {
-            sscanf(buf + 6, "%d", &ms->vm_stk);
-        } else if (strncmp(buf, "VmExe:", 6) == 0) {
-            sscanf(buf + 6, "%d", &ms->vm_exe);
-        } else if (strncmp(buf, "VmLib:", 6) == 0) {
-            sscanf(buf + 6, "%d", &ms->vm_lib);
+            break;
         }
+        //  else if (strncmp(buf, "VmData:", 7) == 0) {
+        //     sscanf(buf + 7, "%d", &ms->vm_data);
+        // } else if (strncmp(buf, "VmStk:", 6) == 0) {
+        //     sscanf(buf + 6, "%d", &ms->vm_stk);
+        // } else if (strncmp(buf, "VmExe:", 6) == 0) {
+        //     sscanf(buf + 6, "%d", &ms->vm_exe);
+        // } else if (strncmp(buf, "VmLib:", 6) == 0) {
+        //     sscanf(buf + 6, "%d", &ms->vm_lib);
+        // }
     }
 
 END:

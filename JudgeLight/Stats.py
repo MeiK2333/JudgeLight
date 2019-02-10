10a7a925321e66cd2b853e5bd4cd529b8c993cc1
MeiK <meik2333@gmail.com>
2019-02-10 22:21:10 +0800

    更新包版本

--- JudgeLight/Stats.py
+++
@@ -1,8 +0,0 @@
-class Stats(object):
-    def __init__(
-        self,
-        time_used,
-        memory_used,
-        message,
-    ):
-        pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(
     name='JudgeLight',
-    version='0.0.1',
+    version='2.0.0',
     ext_modules=[Extension('JudgeLight/JudgeLightRunner', sources=[
         'JudgeLight/JudgeLightRunner/judgelightrunner.c',
         'JudgeLight/JudgeLightRunner/jl_runner.c',
@@ -10,6 +10,15 @@ setup(
         'JudgeLight/JudgeLightRunner/jl_limit.c',
         'JudgeLight/JudgeLightRunner/jl_convert.c',
         'JudgeLight/JudgeLightRunner/jl_rules.c',
+        'JudgeLight/JudgeLightRunner/jl_runner.h',
+        'JudgeLight/JudgeLightRunner/jl_memory.h',
+        'JudgeLight/JudgeLightRunner/jl_limit.h',
+        'JudgeLight/JudgeLightRunner/jl_convert.h',
+        'JudgeLight/JudgeLightRunner/jl_rules.h',
     ])],
-    packages=['JudgeLight']
+    packages=['JudgeLight'],
+    license='MIT',
+    author='MeiK',
+    author_email='meik2333@gmail.com',
+    url='https://github.com/MeiK-h/JudgeLight',
 )
